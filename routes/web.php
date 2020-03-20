8de9ca67c954f6e50f9b9daa20192386ccbed7d2
Dedik Sugiharto <dediksugiharto.com@gmail.com>
2020-03-20 15:34:29 +0700

    Revert "Revert "add new path to book resource""
    This reverts commit 4b9a330636f3b50ccdfaf391c25c6bf5e2b92130.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/books', 'BookController@store');
 Route::patch('/books/{book}', 'BookController@update');
 Route::delete('/books/{book}', 'BookController@destroy');
+
+Route::post('/authors', 'AuthorController@store');
